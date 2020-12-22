fa01f886d6f0affcd65269727beb863310e4f657
sarasandager <sara590x@edu.easj.dk>
2020-12-22 19:45:12 +0100

    assignment 13 GUI added customGUI.fxml working no actions yet

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,6 +8,42 @@
 
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class Controller {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Tab inspirationGalleryTab;
+
+    @FXML
+    private Button showRecipeButton;
+
+    @FXML
+    private TextField recipeNameTextField;
+
+    @FXML
+    private Label chosen;
+
+    @FXML
+    private TextArea ingredientsTextArea;
+
+    @FXML
+    private TextArea instructionsTextArea;
+
+    @FXML
+    void showRecipe(ActionEvent event) {
+
+    }
+
 }
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -18,8 +18,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("customGUI.fxml"));
+        primaryStage.setTitle("The Kogebog - your digital recipes");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
