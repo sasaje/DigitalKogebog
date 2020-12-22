afda20f03d0bde4fbe799284ad92521770db8047
sarasandager <sara590x@edu.easj.dk>
2020-12-22 09:43:45 +0100

    assignment 1

---
+++ src/sample/SimpleTest.java
@@ -0,0 +1,15 @@
+package sample;/*
+ *
+ * Developed by Sara Sandager (sara590x@edu.easj.dk)
+ * Licensed under the MIT License
+ * 22/12/2020
+ *
+ */
+
+public class SimpleTest {
+
+    // 1. Main method in sample.SimpleTest
+    public static void main(String[] args) {
+        System.out.println("Welcome to 'The Kogebog' your digital recipes.");
+    }
+}
